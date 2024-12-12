d87d2e480e5f5b273e668ecfd9b02500a5036bb6
Enderspearl184 <73859349+Enderspearl184@users.noreply.github.com>
2024-12-12 03:57:31 -0800

    Fix Previous Game Results
    This uses a list of CachedPlayerNames because initially I just checked DiscussionPlayerObservations and it caused all non-disconnected players to have empty names.

--- src/DefaultNames.cs
+++ src/DefaultNames.cs
@@ -41,15 +41,10 @@ public class NameList
     public List<string> FullNames;
 }
 
-public class NameGenerator
+public class NameGenerator(NameList names)
 {
-    private NameList Names;
-    private List<string> FullNames;
-    public NameGenerator(NameList names)
-    {
-        Names = names;
-        FullNames = new List<string>(names.FullNames);
-    }
+    private readonly NameList Names = names;
+    private readonly List<string> FullNames = new(names.FullNames);
 
     public string GetRandomName()
     {
@@ -73,15 +68,22 @@ public class NameGenerator
     }
 }
 
+public class CachedPlayerName(string name, int position)
+{
+    public readonly string name = name;
+    public readonly int position = position;
+}
+
 [Mod.SalemMod]
 public class DefaultNames
 {
     static readonly string FolderPath = Path.GetDirectoryName(Application.dataPath) + "/SalemModLoader/ModFolders/DefaultNames/";
     static readonly string NamePath = FolderPath + "Names.json";
     public static NameGenerator generator;
-    public static NameList Names = new();
+    public readonly static NameList Names = new();
     public static bool isCurrentlyPickNames = false;
 
+    public readonly static List<CachedPlayerName> cachedPlayerNames = [];
     public static void Start()
     {
         if (!Directory.Exists(FolderPath))
@@ -94,7 +96,7 @@ public class DefaultNames
             StreamWriter NameFile = File.CreateText(NamePath);
             Names.FirstNames = new(["Cotton", "Deodat", "Edward", "Giles", "James", "James", "John", "John", "John", "Jonathan", "Samuel", "Samuel", "Thomas", "William", "William", "Abigail", "Alice", "Ann", "Ann", "Ann", "Betty", "Dorothy", "Lydia", "Martha", "Mary", "Mary", "Mary", "Sarah", "Sarah"]);
             Names.LastNames = new(["Mather", "Lawson", "Bishop", "Corey", "Bayley", "Russel", "Hathorne", "Proctor", "Willard", "Corwin", "Parris", "Sewall", "Danforth", "Hobbs", "Phips", "Hobbs", "Young", "Hibbins", "Putnam", "Sears", "Parris", "Good", "Dustin", "Corey", "Eastey", "Johnson", "Warren", "Bishop", "Good"]);
-            Names.FullNames = new(["Cotton Mather", "Deodat Lawson", "Edward Bishop", "Giles Corey", "James Bayley", "James Russel", "John Hathorne", "John Proctor", "John Willard", "Jonathan Corw", "Samuel Parris", "Samuel Sewall", "Thomas Danforth", "William Hobbs", "William Phips", "Abigail Hobbs", "Alice Young", "Ann Hibbins", "Ann Putnam", "Ann Sears", "Betty Parris", "Dorothy Good", "Lydia Dustin", "Martha Corey", "Mary Eastey", "Mary Johnson", "Mary Warren", "Sarah Bishop", "Sarah Good", "Sarah Wildes"]);
+            Names.FullNames = new(["Cotton Mather", "Deodat Lawson", "Edward Bishop", "Giles Corey", "James Bayley", "James Russel", "John Hathorne", "John Proctor", "John Willard", "Jonathan Corwin", "Samuel Parris", "Samuel Sewall", "Thomas Danforth", "William Hobbs", "William Phips", "Abigail Hobbs", "Alice Young", "Ann Hibbins", "Ann Putnam", "Ann Sears", "Betty Parris", "Dorothy Good", "Lydia Dustin", "Martha Corey", "Mary Eastey", "Mary Johnson", "Mary Warren", "Sarah Bishop", "Sarah Good", "Sarah Wildes"]);
             NameFile.Write(JsonConvert.SerializeObject(Names, Formatting.Indented));
             NameFile.Close();
         }
@@ -106,43 +108,87 @@ public class DefaultNames
             Names.FullNames = temp.FullNames;
         }
         
-        Debug.Log(JsonConvert.SerializeObject(Names, Formatting.Indented));
+        Debug.Log(JsonConvert.SerializeObject(Names, Formatting.None));
+    }
+}
+
+[HarmonyPatch(typeof(HudLobbyPreviousGamePanel), "OnPreviousGameInfoChanged")]
+public class PreviousGamePatch
+{
+    [HarmonyPriority(Priority.First)]
+    public static void Prefix(PreviousGameData previousGameData)
+    {
+        try
+        {
+            GameObservations observations = Service.Game.Sim.simulation.observations;
+            foreach (PreviousGameEntry entry in previousGameData.entries)
+            {
+                int existingPlayerIndex = DefaultNames.cachedPlayerNames.FindIndex((CachedPlayerName player) => player.position == entry.playerPosition);
+                if (existingPlayerIndex != -1)
+                {
+                    entry.gameName = DefaultNames.cachedPlayerNames[existingPlayerIndex].name;
+                }
+                else if (entry.accountName != Service.Home.UserService.UserInfo.AccountName)
+                {
+                    Debug.LogWarning("DefaultNames was not able to find a CachedPlayerName!");
+                }
+            }
+        } catch (Exception exception)
+        {
+            Debug.LogError("DefaultNames errored!");
+            Debug.LogException(exception);
+        }
     }
 }
 
 [HarmonyPatch(typeof(GameObservations), nameof(GameObservations.HandleDiscussionPlayerObservation))]
 public class GameObservationsPatch
 {
+    [HarmonyPriority(Priority.First)]
     public static void Prefix(DiscussionPlayerObservation discussionPlayerObservation)
     {
-        GameObservations observations = Service.Game.Sim.simulation.observations;
-        DiscussionPlayerState data = discussionPlayerObservation.Data;
-        DiscussionPlayerState myData = observations.myDiscussionPlayer.Data;
-        //make sure we aren't modifying ourselves, and that the packet actually includes an ingame name
-        if (myData.accountName != data.accountName && data.gameName != "")
+        try
         {
-            try
+            GameObservations observations = Service.Game.Sim.simulation.observations;
+            DiscussionPlayerState data = discussionPlayerObservation.Data;
+            //make sure we aren't modifying ourselves, and that the packet actually includes an ingame name
+            if (Service.Home.UserService.UserInfo.AccountName != data.accountName && data.gameName != "")
             {
                 try
                 {
-                    DiscussionPlayerObservation existingObservation = observations.discussionPlayers.Find((DiscussionPlayerObservation player) => player.Data.accountName == data.accountName);
-                    if (existingObservation.Data.gameName != "")
+                    DiscussionPlayerObservation existingObservation = observations.discussionPlayers.Find((DiscussionPlayerObservation player) => player.Data.position == data.position);
+                    if (existingObservation != null && existingObservation.Data.gameName != "")
                     {
                         data.gameName = existingObservation.Data.gameName;
-                    } else
+                    }
+                    else
                     {
                         data.gameName = DefaultNames.generator.GetRandomName();
+
+                        //also, check cachedplayernames and add it if it doesn't exist
+                        if (DefaultNames.cachedPlayerNames.FindIndex((CachedPlayerName player) => player.position == data.position) == -1)
+                        {
+                            Debug.Log("Adding CachedPlayerName " + data.gameName);
+                            DefaultNames.cachedPlayerNames.Add(new CachedPlayerName(data.gameName, data.position));
+                        }
                     }
                 }
                 catch (Exception exception)
                 {
+                    Debug.LogError("DefaultNames errored setting gameName!");
                     Debug.LogException(exception);
                     data.gameName = DefaultNames.generator.GetRandomName();
+                    if (DefaultNames.cachedPlayerNames.FindIndex((CachedPlayerName player) => player.position == data.position) == -1)
+                    {
+                        Debug.Log("Adding CachedPlayerName " + data.gameName);
+                        DefaultNames.cachedPlayerNames.Add(new CachedPlayerName(data.gameName, data.position));
+                    }
                 }
-            } catch (Exception outerException)
-            {
-                Debug.LogException(outerException);
             }
+        } catch (Exception exception)
+        {
+            Debug.LogError("DefaultNames errored!");
+            Debug.LogException(exception);
         }
     }
 }
@@ -154,7 +200,8 @@ class PickNamesPatch
     {
         if (DefaultNames.generator == null)
         {
-            Debug.Log("CREATING NAME GENERATOR");
+            Debug.Log("CREATING NAME GENERATOR AND CLEARING CACHED NAMES");
+            DefaultNames.cachedPlayerNames.Clear();
             DefaultNames.generator = new NameGenerator(DefaultNames.Names);
         }
     }
